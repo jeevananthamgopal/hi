eeb3ff090615c95caea8dd33e03595d2df425adf
jeevananthamgopal <67500021+jeevananthamgopal@users.noreply.github.com>
2023-08-23 13:10:15 +0530

    v1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const microphoneButton = document.getElementById('microphoneButton');
 
 microphoneButton.addEventListener('click',function(){
     window.open(
-        './mic/index1.html',
+        './index1.html',
         "",
         "width=280px,height=280px"
     );
